f94fb8071f6e8930c5bf363b41d248c266d2c362
Thomas Robson <thomasrobson7@gmail.com>
2018-04-22 21:59:47 +0100

    DayNIght integration WIP

---
+++ UnityProject/Assets/Scripts/DayNight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+	public float timeSpeed = 1;
+
+	Skybox sky;
+	Material skyMat;
+
+	void Awake()
+	{
+		skyMat = RenderSettings.skybox;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right * timeSpeed * Time.deltaTime);
+	}
+}
