4e9cab78eea943bea742dbacba5ecf6d8e8897c3
Wolf1938 <38575320+Wolf1938@users.noreply.github.com>
2018-04-22 19:19:08 +0100

    FishyWorking

--- UnityProject/Assets/Scripts/FishController.cs
+++ UnityProject/Assets/Scripts/FishController.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 public class FishController : MonoBehaviour
 {
     private Animator anim;
-    private Rigidbody rb;
-
     private bool dead;
     private float speed = 1;
+    private bool isRunning;
 
 	void Awake ()
     {
         anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody>();
         dead = false;
+        isRunning = false;
         anim.SetBool("Dead", dead);        
 	}
 
     private void FixedUpdate()
     {
-        Debug.Log(rb.velocity);
-
         if (!dead)
         {
-            Transform Parent = gameObject.transform.parent;
+            Transform parent = gameObject.transform.parent;
+            parent.Rotate(Vector3.up, Time.deltaTime * 10 * speed);
         }
+        if (!isRunning)
+        {
+            StartCoroutine(Burst());
+            isRunning = true;
+        }
+        anim.speed = speed;
     }
 
     void Interact ()
     {
         anim.SetBool("Dead", dead);
+    }
+
+    IEnumerator Burst()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        speed = Random.Range(1.1f, 2.5f);
+        yield return new WaitForSeconds(Random.Range(0.5f, 1));
+        speed = 1;
+        isRunning = false;
+        yield return null;
     }
 }
