1396d30e8ff9816a7fffb744db67389e0c00c081
Thomas Robson <thomasrobson7@gmail.com>
2018-04-23 19:58:15 +0100

    MOAR REVERTS

--- UnityProject/Assets/Scripts/FishController.cs
+++ UnityProject/Assets/Scripts/FishController.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class FishController : MonoBehaviour
 {
-    public bool isFish;
-    public int speed = 20;
-
     private Animator anim;
     private bool dead;
     private bool isRunning;
-    private float multiplier = 1;
+    private float speed = 1;
 	private Rigidbody rb;
 
     void Awake()
@@ -18,44 +15,40 @@ public class FishController : MonoBehaviour
         anim = GetComponent<Animator>();
         dead = false;
         isRunning = false;
-        if (isFish)
-        {
-            anim.SetBool("Dead", dead);
-        }
-        rb = GetComponent<Rigidbody>();
+        anim.SetBool("Dead", dead);
+		rb = GetComponent<Rigidbody> ();
     }
 
     private void FixedUpdate()
     {
+
         if (!dead)
         {
             Transform parent = gameObject.transform.parent;
-            parent.Rotate(Vector3.up, Time.deltaTime * speed * multiplier);
+            parent.Rotate(Vector3.up, Time.deltaTime * 20 * speed);
+            //transform.rotation = Quaternion.Euler(0, 0, 0);            
         }
-        if (!isRunning && isFish)
+        if (!isRunning)
         {
             StartCoroutine(Burst());
             isRunning = true;
         }
-        anim.speed = multiplier;
+        anim.speed = speed;
     }
 
     public void Interact()
     {
-        if (isFish)
-        {
-            rb.useGravity = true;
-            dead = true;
-            anim.SetBool("Dead", dead);
-        }
+		rb.useGravity = true;
+		dead = true;
+        anim.SetBool("Dead", dead);
     }
 
     IEnumerator Burst()
     {
         yield return new WaitForSeconds(Random.Range(1, 5));
-        multiplier = Random.Range(1.1f, 2.5f);
+        speed = Random.Range(1.1f, 2.5f);
         yield return new WaitForSeconds(Random.Range(0.5f, 1));
-        multiplier = 1;
+        speed = 1;
         isRunning = false;
         yield return null;
     }
