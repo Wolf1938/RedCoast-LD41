b7ba5abaedd164630eef0e69370a733be17ba427
Wolf1938 <38575320+Wolf1938@users.noreply.github.com>
2018-04-23 19:01:23 +0100

    dating sim update

--- UnityProject/Assets/Scripts/InteractionScript.cs
+++ UnityProject/Assets/Scripts/InteractionScript.cs
@@ -61,7 +61,6 @@ public class InteractionScript : MonoBehaviour {
                     Transform objectHit = hit.transform;
 					if (objectHit != null) 
 					{
-						Debug.Log ("Hit object");
 						if (objectHit.gameObject.name == "Melon-Chan") 
 						{
 							objectHit.GetComponent<DatingSim> ().Interact (itemHeld, gameObject);
